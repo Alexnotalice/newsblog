6c68d7c4b28d7f670274358aa4c1f6c51a7d3124
Alexnotalice <aboutalexz@gmail.com>
2022-02-25 13:43:38 +0800

    api first completed in 0225

--- blogproject/newsapp/views.py
+++ blogproject/newsapp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import requests
 
 
 # Create your views here.
-def test(request):
-    return HttpResponse('testing')
+def web_data(request):
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=212e9c74b6cf77879522202b4ebbbce5&keywords =basketball&countries=us')
+    req = r.json()
+    data = req['data']
+    title = []
+    description = []
+    image = []
+    url = []
+    for i in data:
+        title.append(i['title'])
+        description.append(i['description'])
+        image.append(i['image'])
+        url.append(i['url'])
+    news = zip(title, description, image, url)
+    return render(request, 'news.html', {'news':news})
