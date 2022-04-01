295bdddb88d91a2e407f8f2e599eecb416fc1aa5
Alexnotalice <aboutalexz@gmail.com>
2022-04-01 11:18:33 +0800

    manage.py middleware spelling correction

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoise.Middleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blogproject.urls'
